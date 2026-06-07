83f95c1beb907ca2663f3e80b7bc89659a5926ec
Kabby <kabby.dev@outlook.com>
2026-06-07 17:09:43 +0100

    fix

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -45,7 +45,7 @@ export default function Login() {
     <div className="min-h-screen flex items-center justify-center p-4">
       <div className="w-full max-w-md">
         {/* Form Container */}
-        <div className="bg-white rounded-2xl shadow-lg p-8 backdrop-blur-sm transition-all duration-300 hover:shadow-xl">
+        <div className="rounded-4xl border border-white/10 bg-white/10 backdrop-blur-xl shadow-2xl p-8 transition-all hover:shadow-violet-500/10  duration-300 hover:shadow-xl">
           {/* Header */}
           <div className="mb-8">
             <h1 className="text-lg font-bold text-gray-900 mb-1">
@@ -198,18 +198,24 @@ export default function Login() {
               Apple
             </button>
 
-            <button
-              type="button"
-              className="w-full border-2 border-gray-200 text-gray-700 font-semibold py-2.5 px-4 rounded-lg hover:border-violet-500 hover:bg-violet-50 hover:text-violet-600 transition-all duration-200 flex items-center justify-center gap-2"
-            >
-              <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
-                <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
-                <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
-                <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
-              </svg>
-              Google
-            </button>
+            <svg width="18" height="18" viewBox="0 0 48 48">
+              <path
+                fill="#EA4335"
+                d="M24 9.5c3.54 0 6.72 1.22 9.21 3.6l6.85-6.85C35.9 2.46 30.4 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.2C12.43 13.13 17.81 9.5 24 9.5z"
+              />
+              <path
+                fill="#4285F4"
+                d="M46.1 24.5c0-1.64-.15-3.21-.43-4.75H24v9.02h12.4c-.54 2.9-2.18 5.37-4.66 7.03l7.19 5.6C43.98 37.61 46.1 31.5 46.1 24.5z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M10.54 28.43A14.5 14.5 0 019.5 24c0-1.52.26-2.99.72-4.43l-7.98-6.2A24 24 0 000 24c0 3.87.92 7.53 2.56 10.78l7.98-6.35z"
+              />
+              <path
+                fill="#34A853"
+                d="M24 48c6.48 0 11.93-2.14 15.9-5.82l-7.19-5.6c-2 1.34-4.6 2.12-8.71 2.12-6.19 0-11.57-3.63-13.46-8.93l-7.98 6.35C6.51 42.62 14.62 48 24 48z"
+              />
+            </svg>
           </div>
         </div>
 
